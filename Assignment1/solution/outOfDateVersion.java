9d8a63a53b94159c40405878296d8a6c12a9d47e
anushkachaudhary740 <113453350+anushkachaudhary740@users.noreply.github.com>
2022-09-29 11:16:57 +0530

    Add files via upload

--- Assignment1/solution/outOfDateVersion.java
+++ Assignment1/solution/outOfDateVersion.java
@@ -12,25 +12,42 @@ public class outOfDateVersion {
 
     public HashMap<String,String> findOldVersion() {
         Set<String>st = new HashSet<String>();
-        HashMap<String, String> highest = new HashMap<String, String>();
-        for (Software sft : softwareList) {
-            if (highest.containsKey(sft.getOperatingSystem())) {
-                //highest.replace(sft.getOperatingSystem(), Math.max(sft.getVersion(), highest.get(sft.getOperatingSystem())));
-                Double.toString(Math.max(Double.parseDouble(sft.getVersion()), Double.parseDouble(highest.get(sft.getOperatingSystem()))));
+        HashMap<String,String>highest = new HashMap<String,String>();
+        for (Software sft: softwareList)
+        {
+            if(highest.containsKey(sft.getOperatingSystem()))
+            {
+                String prev = highest.get(sft.getOperatingSystem());
+                String curr = sft.getVersion();
+                StringTokenizer tokens = new StringTokenizer(prev,".");
+                StringTokenizer tokens1 = new StringTokenizer(curr,".");
+                while(tokens.hasMoreTokens()&&tokens1.hasMoreTokens())
+                {
+                    int p = Integer.parseInt(tokens.nextToken());
+                    int c = Integer.parseInt(tokens1.nextToken());
+                    if(p!=c)
+                    {
+                        highest.replace(sft.getOperatingSystem(),p>c?highest.get(sft.getOperatingSystem()):sft.getVersion());
+                    }
+                }
 
-            } else {
+
+            }
+            else
+            {
                 highest.put(sft.getOperatingSystem(), sft.getVersion());
             }
+            //System.out.println(highest.get(sft.getOperatingSystem()));
         }
         return highest;
     }
-public void operatingSystemFinding(HashMap highest){
+public void operatingSystemFinding(HashMap<String,String>highest){
     HashMap<String,Integer>count = new HashMap<String,Integer>();
     Iterator<Software> it1 = softwareList.iterator();
     while(it1.hasNext())
     {
         Software sft = it1.next();
-        if(sft.getVersion().equals(highest.get(sft.getOperatingSystem())))
+        if(!sft.getVersion().equals(highest.get(sft.getOperatingSystem())))
         {
             if(count.containsKey(sft.getOperatingSystem()))
             {
@@ -53,4 +70,5 @@ public void operatingSystemFinding(HashMap highest){
 }
 
 }
+
 
